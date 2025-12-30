b5854c56e771cd705b13744fb9671021e44fe623
richstoremipad <richstoremipad@gmail.com>
2025-12-30 19:06:41 +0700

    Update main.go

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 /* ==========================================
    CONFIG & UPDATE SYSTEM
 ========================================== */
-const AppVersion = "1.4" // Performance Update
+const AppVersion = "1.1" // Performance Update
 const ConfigURL = "https://raw.githubusercontent.com/tangsanrich/Fileku/main/executor.txt"
 const CryptoKey = "RahasiaNegaraJanganSampaiBocorir"
 
